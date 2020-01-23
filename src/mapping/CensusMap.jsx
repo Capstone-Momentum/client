0107702bd7b7431da9074ae85af62e8866adf5cd
Alex Tschopp <altschop@calpoly.edu>
2020-01-23 10:09:40 -0800

    Added tabs for map and graph (#4)

--- src/mapping/CensusMap.jsx
+++ src/mapping/CensusMap.jsx
@@ -2,8 +2,9 @@ import React, { useEffect } from 'react';
 import CensusMapGL from './CensusMapGL';
 import Autocomplete from '../components/autocomplete/Autocomplete';
 import VirtualizedAutocomplete from '../components/autocomplete/VirtualizedAutocomplete'
-import { Typography, CardContent, Card, Divider, Grid, Container, Button } from '@material-ui/core';
+import { Typography, CardContent, Card, Divider, Grid, Container, Button, Tabs, Tab, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+import CensusBarChart from '../charts/line/BarChart';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -21,14 +22,19 @@ const useStyles = makeStyles(theme => ({
     },
     sidebar: {
         padding: '2%',
-        height: '100%'
+        height: '80vh',
+        width: '30vw'
     },
     lowercaseTypography: {
         textTransform: 'lowercase'
     },
     button: {
         color: '#2195F2'
-    }
+    },
+    // container: {
+    //     height: '50%',
+    //     width: '50%',
+    // }
 }))
 
 const availableVintages = [
@@ -59,11 +65,29 @@ const defaultSelection = {
 
 const defaultSelections = [defaultSelection]
 
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <Typography
+            component="div"
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && <Box p={3}>{children}</Box>}
+        </Typography>
+    );
+}
+
 export default function CensusMap(props) {
     const [vintage, setVintage] = React.useState({ vintage: "2016" })
     const [geoLevel, setGeoLevel] = React.useState({ geoLevel: 'county subdivision' })
     const [selection, setSelection] = React.useState(defaultSelection)
     const [selections, setSelections] = React.useState(defaultSelections)
+    const [tab, setTab] = React.useState(0)
     const classes = useStyles()
 
     useEffect(() => {
@@ -93,9 +117,10 @@ export default function CensusMap(props) {
                 <Grid className={classes.outerGridColumn} container direction='column' justify='space-between'>
                     <Grid className={classes.innerGridColumn} item container direction='column' spacing={2}>
                         <Grid item>
-                            <Typography variant='h5'>
-                                Interact with the Map
-                        </Typography>
+                            <Tabs value={tab} onChange={(event, newVal) => setTab(newVal)} indicatorColor="primary">
+                                <Tab label="Map" />
+                                <Tab label="Graph" />
+                            </Tabs>
                         </Grid>
                         <Grid item>
                             <Divider />
@@ -206,12 +231,23 @@ export default function CensusMap(props) {
         </Grid>
     )
 
+    const graph = (<CensusBarChart />);
+
+
+
     return (
-        <Grid className={classes.root} container direction='row' spacing={4}>
+        <Grid className={classes.root} container direction='row' spacing={4} wrap='nowrap'>
             <Grid item xs={4}>
                 {sidebar}
             </Grid>
-            {labeledMap}
+            <Grid item container direction='column' xs={8} spacing={2} alignItems='center' >
+                <Grid item>
+                    <TabPanel value={tab} index={0}>{labeledMap}</TabPanel>
+                </Grid>
+                <Grid item>
+                    <TabPanel value={tab} index={1}>{graph}</TabPanel>
+                </Grid>
+            </Grid>
         </Grid>
     )
 }
