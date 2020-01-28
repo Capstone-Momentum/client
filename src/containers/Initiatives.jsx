85994a7f23cd051643aea7866b50107016095a0f
Jonah Saya <jsaya01@gmail.com>
2020-01-28 10:57:53 -0800

    wire frame of the initiatives page

--- src/containers/Initiatives.jsx
+++ src/containers/Initiatives.jsx
@@ -1,28 +1,47 @@
 import React from 'react';
-import { Grid, List, ListItem, Typography } from '@material-ui/core';
+import { Grid, Paper, List, ListItem, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
     root: {
         height: '20vh',
         padding: '0 4% 0 4%'
-    }
-})
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}))
 
 export default function Initiatives() {
     const classes = useStyles()
 
     return (
-        <Grid className={classes.root} container direction='column' alignItems='center' justify='center'>
-            <Typography variant='h5'> Initiatives </Typography>
-            <List>
-                <ListItem>
-                    <Typography> Aerospace and Defense </Typography>
-                </ListItem>
-                <ListItem>
-                    <Typography> Education </Typography>
-                </ListItem>
-            </List>
-        </Grid>
+        <div>
+            <Typography variant='h5' align='center'> Initiatives </Typography>
+            <Grid className={classes.root} container direction='row' alignItems='center' justify='center'>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>
+                        Aerospace
+                </Paper>
+                </Grid>
+                <Grid item xs={9}>
+                    <Paper className={classes.paper}>
+                        Additional Data
+                </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>
+                        Defense
+                </Paper>
+                </Grid>
+                <Grid item xs={9}>
+                    <Paper className={classes.paper}>
+                        Additional Data
+                </Paper>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
