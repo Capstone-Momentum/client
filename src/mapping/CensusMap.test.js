3eaaea11dd6d109bc6b04f379319a0218554b163
Justin To <justin-to@users.noreply.github.com>
2020-01-29 22:31:34 -0800

    making testcase for getSelectionOptions

--- src/mapping/CensusMap.test.js
+++ src/mapping/CensusMap.test.js
@@ -1,7 +1,13 @@
 import { getSelectionOptions } from './CensusMap';
+import { CensusMap } from './CensusMap';
+import { shallow } from 'enzyme';
 
-describe("CensusMap - Unit Tests", () => {
-    describe('getSelectionOptions', () => {
+// describe("CensusMap - Unit Tests", () => {
+//     describe('getSelectionOptions', () => {
         
-    })
-})
+//     })
+// })
+
+// Want to mock the database to test this function, right now it returns too many things in an array.
+// I will finish this when we have the database worked out.
+
