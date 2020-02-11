6b444f2f02e0e1a146841212f741ec6b796197bf
Justin To <justin-to@users.noreply.github.com>
2020-02-10 23:43:05 -0800

    adding a test for TabPanel

--- src/mapping/CensusMap.test.js
+++ src/mapping/CensusMap.test.js
@@ -1,6 +1,10 @@
-import { getSelectionOptions } from './CensusMap';
+import { getSelectionOptions, TabPanel } from './CensusMap';
 import { CensusMap } from './CensusMap';
-import { shallow } from 'enzyme';
+import { configure, shallow } from 'enzyme';
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 // describe("CensusMap - Unit Tests", () => {
 //     describe('getSelectionOptions', () => {
@@ -9,5 +13,13 @@ import { shallow } from 'enzyme';
 // })
 
 // Want to mock the database to test this function, right now it returns too many things in an array.
-// I will finish this when we have the database worked out.
+// Will finish this when we have the database worked out. 
 
+describe("CensusMap - Unit Tests", () => {
+
+    test('returns correct TabPanel', () => {
+        const wrapper = shallow(<TabPanel children = {'string'} value = {2} index = {1} />)
+        expect(wrapper.props()).toEqual({"aria-labelledby": "simple-tab-1", "children": false, "component": "div", "hidden": true, "id": "simple-tabpanel-1", "role": "tabpanel"});
+    });
+
+}); 
